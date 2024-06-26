94387f790d866fa46437b3a54a076d2dc3f202b8
lucas vittaz <lucasvittaz.pro@gmail.com>
2024-06-26 20:57:50 +0200

    jardica firebase config

---
+++ functions/src/index.ts
@@ -0,0 +1,91 @@
+import cors from 'cors';
+import express = require('express');
+import * as functions from 'firebase-functions';
+import * as fs from 'fs';
+import * as handlebars from 'handlebars';
+import * as nodemailer from 'nodemailer';
+import * as path from 'path';
+
+const app = express();
+app.use(cors({ origin: ['http://localhost:4200', 'https://www.jardica.net', ''] }));
+
+// Configurez le transporteur Nodemailer
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: functions.config().email.user,
+    pass: functions.config().email.pass,
+  },
+});
+
+// Lire et compiler les templates d'email
+const userTemplateSource = fs.readFileSync(
+  path.join(__dirname, 'templates/user-email.html'),
+  'utf8'
+);
+const userTemplate = handlebars.compile(userTemplateSource);
+
+const jardicaTemplateSource = fs.readFileSync(
+  path.join(__dirname, 'templates/jardica-email.html'),
+  'utf8'
+);
+const jardicaTemplate = handlebars.compile(jardicaTemplateSource);
+
+const whitelist = [
+  'http://localhost:4200',
+  'https://www.jardica.net',
+  ''
+];
+
+const corsOptions = {
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (whitelist.indexOf(origin ?? '') !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+const corsHandler = cors(corsOptions);
+
+export const sendContactEmail = functions
+  .region('europe-west1')
+  .https.onRequest((req, res) => {
+    corsHandler(req, res, () => {
+      const { name,  firstName, interventionPlace, category, email, phone, message,  } = req.body;
+
+      // Préparer l'email pour l'utilisateur
+      const userEmailOptions = {
+        from: 'contact@jardica.net',
+        to: email,
+        subject: 'Confirmation de votre demande',
+        html: userTemplate({ name,  firstName, interventionPlace, category, email, phone, message, }),
+      };
+
+      // Préparer l'email pour JARDICA
+      const jardicaEmailOptions = {
+        from: email,
+        to: 'contact@jardica.net',
+        subject: `[${category}] - Nouvelle demande de contact`,
+        html: jardicaTemplate({ name,  firstName, interventionPlace, category, email, phone, message, }),
+      };
+
+      // Envoyer les emails
+      Promise.all([
+        transporter.sendMail(userEmailOptions),
+        transporter.sendMail(jardicaEmailOptions),
+      ])
+        .then(() => {
+          return res
+            .status(200)
+            .json({ message: 'Emails envoyés avec succès' });
+        })
+        .catch((error) => {
+          return res.status(500).json({ error: error.toString() });
+        });
+    });
+  });
